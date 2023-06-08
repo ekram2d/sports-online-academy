18bd97264dcc8eb52733d1eac599911043c4ddcb
Ekram Hossain <ekramhossain117@gmail.com>
2023-06-08 15:59:51 +0600

    populer classes and insrtuction adde

--- src/Components/Home/Home/Home.jsx
+++ src/Components/Home/Home/Home.jsx
@@ -2,21 +2,44 @@ import React from 'react';
 
 import Slider from './Slider/Slider';
 import usemenu from '../../../Hooks/usemenu';
+import ClaseeDes from './ClaseeDes';
+import Instructors from './Instructors';
 
 
 const Home = () => {
-      const data=usemenu();
-      // console.log(data)
+      const data = usemenu();
+      console.log(data)
       return (
-            <div className=''>
-                 
-               <Slider></Slider>
-                  
-            </div>
+            <>
 
-            
+                  <Slider></Slider>
+                  <h1 className='text-center text-2xl m-3 font-bold'>Popular Classes Section</h1>
+                  <div className='lg:grid lg:grid-cols-2 gap-3 '>
+                       
+                        {
+                              data?.slice(0, 6).map((data, index) =>
 
-            
+                                    <ClaseeDes key={index + 1} data={data}></ClaseeDes>
+                              )
+                        }
+                  </div>
+                  <h1 className='text-center text-2xl m-3 font-bold'>Popular Instructors Section</h1>
+                  <div className='lg:grid lg:grid-cols-2 gap-3 '>
+                        {
+                              data?.slice(0, 6).map((data, index) =>
+
+                                    <Instructors key={index + 1} data={data}></Instructors>
+                              )
+                        }
+                  </div>
+
+            </>
+
+
+
+
+
+
       );
 };
 
