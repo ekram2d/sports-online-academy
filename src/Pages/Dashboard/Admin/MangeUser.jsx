6ac42099586d6529f4d62ca3ddbb42e6c469046b
Ekram Hossain <ekramhossain117@gmail.com>
2023-06-09 18:14:35 +0600

    admin or instructor part done

--- src/Pages/Dashboard/Admin/MangeUser.jsx
+++ src/Pages/Dashboard/Admin/MangeUser.jsx
@@ -1,28 +1,132 @@
 import React, { useContext } from 'react';
 import { AuthContext } from '../../../providers/Authprovider';
 import { useQuery } from '@tanstack/react-query';
-
+import { BiUserCheck,BiUserCircle} from "react-icons/bi";
+import Swal from 'sweetalert2';
 const MangeUser = () => {
 
-      const {user,loading}=useContext(AuthContext);
-      const { isLoading,refetch, data:userscart=[], error } = useQuery({
+      const { user, loading } = useContext(AuthContext);
+      const { isLoading, refetch, data: userscart = [], error } = useQuery({
             queryKey: ['users'],
             enabled: !loading,
-              queryFn: async () => {
-      
-                        const response = await fetch('http://localhost:5001/users')
-                        // , {
-                        //       headers: {
-                        //             authorization: `bearer ${token}`
-                        //       }
-                        // })
-                        return response.json()
-                  },
-          
+            queryFn: async () => {
+
+                  const response = await fetch('http://localhost:5001/users')
+                  // , {
+                  //       headers: {
+                  //             authorization: `bearer ${token}`
+                  //       }
+                  // })
+                  return response.json()
+            },
+
       })
+      const handleAdmin=(id,data)=>{
+            // console.log(id);
+
+            const data1 = {
+                  field1: data, // Replace 'new_value1' with the new value for field1
+                  
+                };
+                
+                fetch(`http://localhost:5001/users/admin/${id}`, {
+                  method: 'PATCH',
+                  headers: {
+                    'Content-Type': 'application/json',
+                  },
+                  body: JSON.stringify(data1),
+                })
+                  .then(response => response.json())
+                  .then(updatedUser => {
+                      
+                    console.log('User updated:', updatedUser.modifiedCount);
+                    if(updatedUser.modifiedCount >=1){
+                        refetch();
+                        Swal.fire({
+                              position: 'top-end',
+                              icon: 'success',
+                              title: `Your are now ${data}`,
+                              showConfirmButton: false,
+                              timer: 1500
+                            })
+                    }
+                    // Handle the updated user data as needed
+                  })
+                  .catch(error => {
+                    console.error('Error updating user:', error);
+                    // Handle errors
+                  });
+      }
+
+      // console.log(userscart)
       return (
-            <div className='w-full m-2'>
-                   manage user admin:{userscart.length}
+            <div className='w-full ms-2'>
+                 <p className='text-center uppercase font-serif text-black'> manage users : {userscart.length}</p>
+
+                  <div className="overflow-x-auto">
+                        <table className="table">
+                              {/* head */}
+                              <thead>
+                                    <tr className='text-black font-bold'>
+                                          <th>
+                                               #
+                                          </th>
+                                          <th>Name</th>
+                                          <th>email</th>
+                                          <th>role</th>
+                                          <th>Make Admin</th>
+                                          <th>Make Instructor</th>
+                                    </tr>
+                              </thead>
+                              <tbody>
+                                    {/* row 1 */}
+                                    {
+                                          userscart?.map((user,index)=>
+                                                <tr key={user._id}>
+                                                <th>
+                                                     {index+1}
+                                                </th>
+                                                <td>
+                                                      <div className="flex items-center space-x-3">
+                                                            
+                                                            <div>
+                                                                  <div className="">{user?.name}</div>
+                                                                  
+                                                            </div>
+                                                      </div>
+                                                </td>
+                                                <td>
+                                                      {user?.email}
+                                                    
+                                                </td>
+                                                <td>{user?.role}</td>
+                                                <th>
+                                                     {
+                                                      user?.role !='admin' ? <button onClick={()=>handleAdmin(user?._id,'admin')} className="btn btn-primary btn-sm">
+                                                        <BiUserCircle></BiUserCircle>
+
+                                                      </button>: <button  className="btn btn-sm bg-white btn-disabled"> <BiUserCircle></BiUserCircle></button>
+                                                     }
+                                                </th>
+                                                <th>
+                                                {
+                                                      user?.role !='instructor' ? <button onClick={()=>handleAdmin(user?._id,'instructor')} className="btn btn-primary btn-sm">
+                                                            <BiUserCheck></BiUserCheck>
+                                                      </button>: <button  className="btn btn-sm bg-white  btn-disabled"><BiUserCheck></BiUserCheck></button>
+                                                     }
+                                                </th>
+                                          </tr>
+
+                                          )
+                                    }
+                                   
+                                    {/* row 2 */}
+                                   
+                              </tbody>
+                             
+
+                        </table>
+                  </div>
             </div>
       );
 };
